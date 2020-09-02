193014e7440f488de6bc9e360a147ddc25c45426
John <john383p@stud.kea.dk>
2020-09-02 13:25:00 +0200

    Blah2

--- src/com/company/Second.java
+++ src/com/company/Second.java
@@ -2,6 +2,18 @@ package com.company;
 import java.util.ArrayList;
 public class Second
 {
+    public void testDoesArray()
+    {
+        ArrayList<String> sejesteTestListeEver = new ArrayList<String>();
+
+        sejesteTestListeEver.add("Torben");
+        sejesteTestListeEver.add("Iben");
+        sejesteTestListeEver.add("Magnus");
+        sejesteTestListeEver.add("Bo");
+
+        System.out.println(doesArraylistContainString(sejesteTestListeEver, "Bo"));
+    }
+
     public boolean doesArraylistContainString(ArrayList<String> list, String word)
     {
         for(String s : list)
